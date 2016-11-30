29460b65b2464a6bd0349f66bf0e4aee7645f514
ytmfdw <164044169@qq.com>
2016-12-01 00:48:34 +0800

    修正主界面框架问题

--- 千锋办公OA/src/main/java/oa/qianfeng/com/oa/ui/activity/MainActivity.java
+++ 千锋办公OA/src/main/java/oa/qianfeng/com/oa/ui/activity/MainActivity.java
@@ -1,13 +1,14 @@
 package oa.qianfeng.com.oa.ui.activity;
 
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TabHost;
 import android.widget.TextView;
 
@@ -19,7 +20,7 @@ import oa.qianfeng.com.oa.ui.fragment.BaseNetFragment;
 import oa.qianfeng.com.oa.ui.fragment.MessageFragment;
 import oa.qianfeng.com.oa.ui.fragment.MineFragment;
 
-public class MainActivity extends BaseNetActivity {
+public class MainActivity extends BaseNetActivity implements TabHost.OnTabChangeListener, ViewPager.OnPageChangeListener {
 
     String[] titles = {
             "我的", "考勤", "公告"
@@ -34,6 +35,9 @@ public class MainActivity extends BaseNetActivity {
     ViewPager viewpager;
     @BindView(R.id.tabhost)
     FragmentTabHost tabhost;
+
+    LayoutInflater inflater;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,22 +66,53 @@ public class MainActivity extends BaseNetActivity {
         };
         viewpager.setAdapter(adapter);
 
+        viewpager.addOnPageChangeListener(this);
+
     }
 
     private void setupTabHost() {
         tabhost.setup(this, getSupportFragmentManager(), R.id.viewpager);
+        inflater = LayoutInflater.from(this);
+
         for (int i = 0; i < fragments.length; i++) {
             TabHost.TabSpec tab = tabhost.newTabSpec("" + i);
             tab.setIndicator(getTahView(i));
             tabhost.addTab(tab, tabhostClasses[i], null);
             tabhost.getTabWidget().setDividerDrawable(new ColorDrawable(0x00000000));
         }
+
+        tabhost.setOnTabChangedListener(this);
     }
 
     private View getTahView(int i) {
+
+        View view = inflater.inflate(R.layout.tabhost_item, null);
         //创建TabView
-        TextView tv = new TextView(this);
+        ImageView iv = (ImageView) view.findViewById(R.id.iv_tabimg);
+
+        TextView tv = (TextView) view.findViewById(R.id.tv_tabtext);
         tv.setText(titles[i]);
-        return tv;
+        return view;
+    }
+
+    @Override
+    public void onTabChanged(String tabId) {
+        int index = Integer.valueOf(tabId);
+        viewpager.setCurrentItem(index);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        tabhost.setCurrentTab(position);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
     }
 }
